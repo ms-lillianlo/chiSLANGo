d8b97b3187439488b43508e1f377e26244017a7d
Lillian Wakefield <lillianwakefield@gmail.com>
2018-08-14 17:47:47 -0500

    routes added

---
+++ routes/index.js
@@ -0,0 +1,107 @@
+var express = require('express');
+var router = express.Router();
+const models = require('../models');
+
+const app = express();
+app.use(express.static('public'));
+
+
+// serve the homepage
+app.get('/home', (req, res) => {
+  res.sendFile(__dirname + '/home.hbs');
+});
+
+//const setupAuth = require('./auth');
+const shuffleArray = array => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Chislango' });
+});
+
+router.get('/login', function (req, res, next) {
+  //res.render('login', { title: 'stuff here for github' });
+});
+
+let shuffledQuestions;
+let answers;
+let score = 0;
+let previousQuestion;
+let remainQuestions;
+let tryAgain= "disabled";
+let tyButton;
+let continueButton;
+
+
+router.get('/home', function(req, res, next) {
+ // res.sendFile(__dirname + '/index.hbs');
+
+  models.Question.findAll()
+  .then(questions => {
+    shuffleArray(questions);
+    shuffledQuestions = questions;
+    answers = [questions[0].option_1, questions[0].option_2, questions[0].option_3, questions[0].correct_answer];
+    shuffleArray(answers);
+
+    res.render('home', {
+      phrase: questions[0].phrase,
+      literal_translation: questions[0].literal_translation,
+      answer1: answers[0],
+      answer2: answers[1],
+      answer3: answers[2],
+      answer4: answers[3],
+      score: score,
+      tryAgain: tryAgain 
+    });
+  });
+});
+
+router.post('/home', function(req, res, next) {
+  
+  previousQuestion = shuffledQuestions.shift();
+
+  if(shuffledQuestions.length == 0){
+    if(req.body.answer == previousQuestion.correct_answer){score += 1;}
+    res.render('endGame', {
+      answerStatus: "End of questions!",
+      score: score
+    });
+    score = 0;
+  }
+  if(req.body.answer == previousQuestion.correct_answer){
+    score += 1;
+    answerStatus= "Correct!";
+    tryAgain= "disabled";
+}
+else {
+  answerStatus= "Incorrect";
+  tryAgain= "";
+}
+
+  shuffleArray(shuffledQuestions);
+  answers = [shuffledQuestions[0].option_1, shuffledQuestions[0].option_2, shuffledQuestions[0].option_3, shuffledQuestions[0].correct_answer];
+  shuffleArray(answers);
+
+  
+  res.render('home', {
+    phrase: shuffledQuestions[0].phrase,
+    literal_translation: shuffledQuestions[0].literal_translation,
+      answer1: answers[0],
+      answer2: answers[1],
+      answer3: answers[2],
+      answer4: answers[3],
+      score: score, 
+      answerStatus: answerStatus,
+      tryagain: tryAgain
+  });
+
+});
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+const ensureAuthenticated = require('../auth').ensureAuthenticated;
+const User = require('../models/user');
+
+router.all('*', ensureAuthenticated);
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
