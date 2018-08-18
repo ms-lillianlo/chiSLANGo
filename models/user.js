d26cd214c62f128ed7003e514a9d54da13fc2068
James Gottschalk <jgottschalk@riskalyze.com>
2018-08-18 10:18:59 -0400

    switched auth to github

--- models/user.js
+++ models/user.js
@@ -1,12 +1,14 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  var User = sequelize.define('User', {
-    username: DataTypes.STRING,
-
-    facebookid: DataTypes.STRING,
-
-    password: DataTypes.STRING
-  }, {});
+  var User = sequelize.define(
+    "User",
+    {
+      username: DataTypes.STRING,
+      githubid: DataTypes.STRING,
+      password: DataTypes.STRING
+    },
+    {}
+  );
   User.associate = function(models) {
     // associations can be defined here
     User.hasMany(models.Answer);
